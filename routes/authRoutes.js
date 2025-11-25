3348bc96d7a98caa9c66ca80fe45e2902ac12ec3
emmeiwhite <emmeiwhite@gmail.com>
2025-11-25 19:52:49 +0530

    chore: Create routes | register and login

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,4 +4,14 @@ import express from 'express'
 
 const router = express.Router()
 
+// 1. REGISTER ROUTE
+router.post('/register', (req, res) => {
+  // Business Logic
+})
+
+// 2. LOGIN ROUTE
+router.post('/login', (req, res) => {
+  // Business Logic
+})
+
 export default router
