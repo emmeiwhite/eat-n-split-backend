42f9271e6286ad787085f2ce5266688bd93f15e9
emmeiwhite <emmeiwhite@gmail.com>
2025-11-25 20:48:48 +0530

    chore: Validate fields , Check if user exists

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,6 +1,21 @@
-export async function registerUser(req, res) {
-  // Business logic Here for user register
+import { User } from '../models/User.js'
+import bcrypt from 'bcryptjs'
+
+export const registerUser = async (req, res) => {
   try {
+    const { name, email, password } = req.body
+
+    // 1. Basic Validation
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: 'Please provide all fields' })
+    }
+
+    //   2. Check if email already exists
+    const existingUser = await User.findOne({ email })
+    if (existingUser) {
+      return res.status(400).json({ message: 'User already exists' })
+    }
+
     res.status(200).json({ message: 'Register endpoint hit' })
   } catch (error) {
     res.status(500).json({ message: 'Server error' })
